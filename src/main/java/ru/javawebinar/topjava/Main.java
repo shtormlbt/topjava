1a22a5803f93c41aee4c7e1ae1c3e62a994e4655
Leonid K <shtromlbt@mail.ru>
2020-09-23 21:59:12 +0500

    HW0 test commit

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -5,6 +5,7 @@ package ru.javawebinar.topjava;
  * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
  */
 public class Main {
+    //test commit and push
     public static void main(String[] args) {
         System.out.format("Hello TopJava Enterprise!");
     }
