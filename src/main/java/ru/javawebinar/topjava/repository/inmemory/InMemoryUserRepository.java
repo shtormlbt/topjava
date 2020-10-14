8bef7a16b1d1f0da948ae4a07eafea380ed7b6b9
Leonid K <shtromlbt@mail.ru>
2020-10-14 22:15:26 +0500

    HW2 сортировка пользователей name - email

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -54,15 +54,21 @@ public class InMemoryUserRepository implements UserRepository {
                 new Comparator<User>() {
                     @Override
                     public int compare(User o1, User o2) {
-                        if(o1.getId()==o2.getId()){
+                        if(o1.getName().compareTo(o2.getName())==0){
                             return o1.getEmail().compareTo(o2.getEmail());
                         }else{
-                            if(o1.getId()>o2.getId()){
-                                return 1;
-                            }else{
-                                return -1;
-                            }
+                            return o1.getName().compareTo(o2.getName());
                         }
+
+//                        if(o1.getId()==o2.getId()){
+//                            return o1.getEmail().compareTo(o2.getEmail());
+//                        }else{
+//                            if(o1.getId()>o2.getId()){
+//                                return 1;
+//                            }else{
+//                                return -1;
+//                            }
+//                        }
 
                     }
                 }
