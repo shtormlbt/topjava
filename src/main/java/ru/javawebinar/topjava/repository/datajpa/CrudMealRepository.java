7e8f62c2218aaef4bd944b0a86587a95f89fa7f3
Leonid K <shtromlbt@mail.ru>
2020-11-02 21:26:44 +0500

    HW05-1

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,25 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    Meal getByIdAndUserId(Integer id,Integer userId);
+
+
+    Meal getById(Integer id);
+    List<Meal> getAllByUserIdOrderByDateTimeDesc(Integer userId);
+
+    @Query(
+            value = "SELECT m FROM Meal m WHERE m.user.id = :userId AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime ORDER BY m.dateTime DESC"
+    )
+    List<Meal> getDateTimeDateBetweenStartDateAndStopDate(@Param("userId")int userId, @Param("startDateTime")LocalDateTime startDateTime, @Param("endDateTime")LocalDateTime endDateTime);
+
+
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,7 +1,9 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
@@ -16,28 +18,46 @@ public class DataJpaMealRepository implements MealRepository {
         this.crudRepository = crudRepository;
     }
 
+    @Autowired
+    private DataJpaUserRepository userRepository;
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        User user = userRepository.get(userId);
+        meal.setUser(user);
+        if(meal.isNew()){
+            return crudRepository.save(meal);
+        }else if(crudRepository.getByIdAndUserId(meal.getId(),userId)==null){
+            return null;
+        }
+        return crudRepository.save(meal);
+
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        //Meal meal = crudRepository.getById(id);
+        Meal delMeal = crudRepository.getByIdAndUserId(id,userId);
+        if(delMeal!=null){
+            crudRepository.delete(delMeal);
+            return true;
+        }else{
+            return false;
+        }
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.getByIdAndUserId(id,userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return  crudRepository.getAllByUserIdOrderByDateTimeDesc(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudRepository.getDateTimeDateBetweenStartDateAndStopDate(userId,startDateTime,endDateTime);
     }
 }
